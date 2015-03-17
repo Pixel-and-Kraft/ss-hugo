3050bba375a65d3ef96fe9c2d105ffdfb4d43ee5
isaac <mulchkinn@gmail.com>
2015-03-17 12:15:53 +0300

    moving assets

--- static-src/assets/js/bundle/helpers.js
+++
@@ -1,8 +0,0 @@
-exports.helloWorld = function() {
-  console.log('hello world!')
-  var test = "test"
-  if (test === "test") {
-    console.log('true!')
-    console.log('hi from helpers.js')
-  }
-}
